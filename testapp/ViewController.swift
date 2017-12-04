cee759d0b25bf5a541eee82a16c4dc807a48f702
Tim Stans <tim@stans-it.nl>
2017-12-04 14:44:29 +0100

    Initial Commit

---
+++ testapp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testapp
+//
+//  Created by Tim Stans on 04-12-17.
+//  Copyright © 2017 Spierings Mobile Cranes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
